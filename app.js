b0769991549f74eed8d10d7a12c390d59b387d05
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-20 00:27:35 +0300

    feat: complete human choice function
    add promise to correctly resolve the human function allowing the correct
    value to be captured after the user confirm their choice. after
    confirmation the choosing menu disappears and another game menu will be
    added

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-let getCompChoice = function(){
+let getCompChoice = async function(){
 
-    let compChoice = Math.floor(Math.random()*3);
-    let compChoiceStr = "";
-
-    switch (compChoice) {
-        case 0:
-            compChoiceStr = "Rock";
-            break;
-        case 1:
-            compChoiceStr = "Paper";
-            break;
-        case 2:
-            compChoiceStr = "Scissors";
-            break;
-        default:
-            compChoiceStr = "Paper";
-    }
-
-    return compChoiceStr;
+    return new Promise((resolve)=>{
+        let compChoice = Math.floor(Math.random()*3);
+        let compChoiceStr = "";
+    
+        switch (compChoice) {
+            case 0:
+                compChoiceStr = "Rock";
+                break;
+            case 1:
+                compChoiceStr = "Paper";
+                break;
+            case 2:
+                compChoiceStr = "Scissors";
+                break;
+            default:
+                compChoiceStr = "Paper";
+        }
+    
+        resolve(compChoiceStr);
+    });
 }
 
 let getHumanChoice = function(){
+    return new Promise((resolve) => {
     let imageLinks = document.querySelectorAll(".image-link");
     let MainContainer = document.querySelector(".flex-cont");
+    let humanChoice = "";
     
     for (const link of imageLinks) {
         link.addEventListener("click",(ev)=>{
             //prevent page reload
             ev.preventDefault();
-            let humanChoice = link.childNodes[0].alt;
+            humanChoice = link.childNodes[0].alt;
     
             let oldConfirmationMessage;
     
@@ -56,19 +60,31 @@ let getHumanChoice = function(){
 
             noButton.addEventListener("click",(ev)=>{
                 ev.preventDefault();
-                    console.log("la2");
                     MainContainer.removeChild(confirmationMessage);
             });
 
             yesButton.addEventListener("click",(ev)=>{
                 ev.preventDefault();
-                    console.log("ah");
+                MainContainer.classList.add("fade-out");
+                MainContainer.addEventListener("transitionend", () => 
+                    {
+                        document.body.removeChild(MainContainer);
+                        resolve(humanChoice);
+                    });
             });
-    
+
             
         });
     }
+});
 }
 
-getCompChoice();
-getHumanChoice();
+
+let startGame = async () => {
+    let compChoice = await getCompChoice();
+    let humanChoice = await getHumanChoice();
+}
+
+startGame();
+
+
