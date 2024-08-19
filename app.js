bfb613327b499d9252c81279b7c3ef711cba471e
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-19 12:33:02 +0300

    feat: add pictures and confirmation message
    the three pictures added represent rock paper and scissors along with a
    confirmation message after the image is clicked to give user the ability
    to change the input

--- app.js
+++ app.js
@@ -20,5 +20,55 @@ let getCompChoice = function(){
     return compChoiceStr;
 }
 
+let getHumanChoice = function(){
+    let imageLinks = document.querySelectorAll(".image-link");
+    let MainContainer = document.querySelector(".flex-cont");
+    
+    for (const link of imageLinks) {
+        link.addEventListener("click",(ev)=>{
+            //prevent page reload
+            ev.preventDefault();
+            let humanChoice = link.childNodes[0].alt;
+    
+            let oldConfirmationMessage;
+    
+            if(document.querySelector(".conf-message")){
+                oldConfirmationMessage = document.querySelector(".conf-message");
+            }
+    
+            let confirmationMessage = document.createElement("div");
+            confirmationMessage.classList.add("conf-message");
+            confirmationMessage.innerHTML = `Are you sure you want to pick ${humanChoice}
+            <div class="Yes-or-no-cont">
+                <a href="/" class="yes-cont"><button class="yes-button">Yes</button></a>
+                <a href="/" class="no-cont"><button class="no-button">No</button></a>
+            </div>
+            `;
+    
+            if(oldConfirmationMessage){
+                MainContainer.removeChild(oldConfirmationMessage);
+            }
+    
+            MainContainer.append(confirmationMessage);
 
-console.log(getCompChoice());
+            let yesButton = document.querySelector(".yes-button");
+            let noButton = document.querySelector(".no-button");
+
+            noButton.addEventListener("click",(ev)=>{
+                ev.preventDefault();
+                    console.log("la2");
+                    MainContainer.removeChild(confirmationMessage);
+            });
+
+            yesButton.addEventListener("click",(ev)=>{
+                ev.preventDefault();
+                    console.log("ah");
+            });
+    
+            
+        });
+    }
+}
+
+getCompChoice();
+getHumanChoice();
