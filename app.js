bef32c8e1c0da9b5c1e8f2c6ceb557de73a0d115
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-23 13:14:17 +0300

    feat: add game logic and styling edits
    game logic was added with basic logging to show winner
    and added a way to hide and show the picking menu to be able to play
    rounds in the future which is the next thing to do

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ let getCompChoice = async function(){
     
         switch (compChoice) {
             case 0:
-                compChoiceStr = "Rock";
+                compChoiceStr = "rock";
                 break;
             case 1:
-                compChoiceStr = "Paper";
+                compChoiceStr = "paper";
                 break;
             case 2:
-                compChoiceStr = "Scissors";
+                compChoiceStr = "scissors";
                 break;
             default:
-                compChoiceStr = "Paper";
+                compChoiceStr = "paper";
         }
     
         resolve(compChoiceStr);
@@ -33,6 +33,11 @@ let getHumanChoice = function(){
             //prevent page reload
             ev.preventDefault();
             humanChoice = link.childNodes[0].alt;
+
+            for (const link of imageLinks) {
+                link.classList.remove("selected");
+            }
+            link.classList.add("selected");
     
             let oldConfirmationMessage;
     
@@ -61,16 +66,18 @@ let getHumanChoice = function(){
             noButton.addEventListener("click",(ev)=>{
                 ev.preventDefault();
                     MainContainer.removeChild(confirmationMessage);
+                    link.classList.remove("selected");
             });
 
             yesButton.addEventListener("click",(ev)=>{
                 ev.preventDefault();
                 MainContainer.classList.add("fade-out");
                 MainContainer.addEventListener("transitionend", () => 
-                    {
-                        document.body.removeChild(MainContainer);
+                    {   
+                        link.classList.remove("selected");
+                        MainContainer.style.display = "none";
                         resolve(humanChoice);
-                    });
+                    },{once:true});
             });
 
             
@@ -79,10 +86,74 @@ let getHumanChoice = function(){
 });
 }
 
+let gameLogic = async function(humanChoice,compChoice){
+    if (humanChoice === "rock") {
+        if (compChoice === "scissors") {
+            return "win";
+        } 
+        else if(compChoice === "paper"){
+            return "lose";
+        }
+        else if(compChoice === "rock"){
+            return "tie";
+        }
+    } else if (humanChoice === "paper") {
+        if (compChoice === "scissors") {
+            return "lose";
+        } 
+        else if(compChoice === "paper"){
+            return "tie";
+        }
+        else if(compChoice === "rock"){
+            return "win";
+        }
+    } else  { //scissors
+        if (compChoice === "scissors") {
+            return "tie";
+        } 
+        else if(compChoice === "paper"){
+            return "win";
+        }
+        else if(compChoice === "rock"){
+            return "lose";
+        }
+    } 
+}
+
 
 let startGame = async () => {
+    let humanScore =0;
+    let compScore = 0;
     let compChoice = await getCompChoice();
     let humanChoice = await getHumanChoice();
+
+    console.log(humanChoice,compChoice);
+    
+    let winner = await gameLogic(humanChoice.toLowerCase(),compChoice.toLowerCase());
+
+    let imageChoicesCont = document.createElement("div");
+        imageChoicesCont.classList.add("conf-message");
+        imageChoicesCont.innerHTML = `Are you sure you want to pick ${humanChoice}
+            <div class="Yes-or-no-cont">
+                <a href="/" class="yes-cont"><button class="yes-button">Yes</button></a>
+                <a href="/" class="no-cont"><button class="no-button">No</button></a>
+            </div>
+            `;
+    
+    // document.body.append(imageChoicesCont);
+
+    console.log(winner);
+    
+    if(winner === "win"){
+        console.log(`You win, ${humanChoice} beats ${compChoice}`);
+    } else if(winner === "lose"){
+        console.log(`You lose, ${compChoice} beats ${humanChoice}`);
+    } else {
+        console.log("Tie ya negm");   
+    }
+
+    document.querySelector(".flex-cont").style.display = "";
+    document.querySelector(".flex-cont").classList.remove("fade-out");
 }
 
 startGame();
